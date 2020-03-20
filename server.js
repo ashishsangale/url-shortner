798ae532e533dc270d3b7d40c304d25f768bbb69
Ashish Sangale <aashishsangale07@gmail.com>
2020-03-21 00:05:27 +0530

    mongo connection

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl')
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false}))
 
-app.get("/", (req,res) => {
-    res.render("index")
-})
+mongoose.connect('mongodb://localhost/urlShortner', {
+    useNewUrlParser: true, useUnifiedTopology: true
+});
+
+app.get("/",async (req,res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render("index", {shortUrls:shortUrls})
+});
+
+app.post("/shortUrls", async (req,res) => {
+    await ShortUrl.create({full: req.body.fullUrl })
+
+    res.redirect('/')
+});
+
+app.get("/:shortUrl", async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+    if(shortUrl == null) return res.sendStatus(404)
+
+    shortUrl.clicks++
+    shortUrl.save()
+
+    res.redirect(shortUrl.full)
+});
 
 app.listen(3000, process.env.IP);
